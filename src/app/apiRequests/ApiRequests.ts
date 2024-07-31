0e61c00b011c703b1e862833b7a40d19e7c4e2c3
Monchusay <armydolgov@mail.ru>
2024-07-31 13:04:44 +0300

    ghPages release Mixed Content error fix attempt

--- src/app/apiRequests/ApiRequests.ts
+++ src/app/apiRequests/ApiRequests.ts
@@ -6,7 +6,7 @@ export const fetchCountersApi: (
   offset: number
 ) => Promise<FetchResultType> = async (limit, offset) => {
   const response: FetchResultType = await fetch(
-    `http://showroom.eis24.me/api/v4/test/meters?limit=${limit}&offset=${offset}`
+    `https://showroom.eis24.me/api/v4/test/meters?limit=${limit}&offset=${offset}`
   ).then((res) => res.json());
   return response;
 };
@@ -14,12 +14,12 @@ export const fetchCounterAddress: (
   id: string
 ) => Promise<CounterAddressType> = async (id) => {
   const response: CounterAddressType = await fetch(
-    `http://showroom.eis24.me/api/v4/test/areas/${id}`
+    `https://showroom.eis24.me/api/v4/test/areas/${id}`
   ).then((res) => res.json());
   return response;
 };
 export const deleteCounterById = async (id: string) => {
-  return await fetch(`http://showroom.eis24.me/api/v4/test/meters/${id}/`, {
+  return await fetch(`https://showroom.eis24.me/api/v4/test/meters/${id}/`, {
     method: 'DELETE',
   });
 };
